36ee70d74db7ba63656be6e7a7ab6ff43f9df293
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-09-18 11:43:53 +0530

    second commit

--- closures.js
+++ closures.js
@@ -22,14 +22,33 @@
 //   }
 //   console.log(x);
   
-function makeFunc() {
-    const name = "Mozilla";
-    displayName() {
-      console.log(name);
-    }
-    return displayName;
-  }
+// function makeFunc() {
+//     const name = "Mozilla";
+//     function displayName() {
+//       console.log(name);
+//     }
+//     return displayName;
+//   }
   
-  const myFunc = makeFunc();
-  myFunc();
+//   const myFunc = makeFunc();
+//   myFunc();
   
+// function makeAdder(x){
+//     return function(y){
+//         return x+y;
+//     }
+// }
+
+// const add5 = makeAdder(5);
+// console.log(add5(3)); // Outputs: 8
+// const add10 = makeAdder(10);
+// console.log(add10(2)); // Outputs: 12
+ // Outputs: 6
+
+
+let name = "Mozilla";
+function displayName() {
+  console.log(name);
+}
+name = "Akhil"
+displayName(); // Outputs: Akhil
